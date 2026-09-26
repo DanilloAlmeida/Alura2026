a5e9dde89a9b91a4d50333d1ae466e8610562a28
DanilloAlmeida <danillodca@gmail.com>
2026-09-26 06:49:27 -0300

    adicionando alterar estado

--- curso-python-1-aula_3/curso-python-1-aula_3/app.py
+++ curso-python-1-aula_3/curso-python-1-aula_3/app.py
@@ -42,10 +42,32 @@ def exibir_subtitulo(texto):
 def cadastrar_novo_restaurante():
     exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')     
+    dados_do_restaurante = {'nome': nome_do_restaurante, 'categoria': categoria, 'ativo': False}
+    restaurantes.append(dados_do_restaurante) 
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
     
     voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+    exibir_subtitulo('Alterar estado do restaurante')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if restaurante['nome'].lower() == nome_do_restaurante.lower():
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            if restaurante['ativo']:
+                mensagem = f'O restaurante {restaurante["nome"]} foi ativado com sucesso!' 
+            else:
+                mensagem = f'O restaurante {restaurante["nome"]} foi desativado com sucesso!'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print(f'O restaurante {nome_do_restaurante} não foi encontrado!')
+    
+    
+    
 
 def listar_restaurantes():
     exibir_subtitulo('Listando restaurantes')
